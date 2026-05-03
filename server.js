bf68d24a7e7aec75663c43e3d5c171db101e9c88
lazargrasy <lasargrasy@gmail.com>
2026-05-03 22:01:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,13 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
+if (process.env.CLOUDINARY_CLOUD_NAME) {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  });
+}
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
